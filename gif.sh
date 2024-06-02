17d86774e1a6f405f08b979524bbbc1bcc568eec
cbingb666 <33190523+cbingb666@users.noreply.github.com>
2024-06-03 03:11:56 +0800

    Update gif.sh

--- gif.sh
+++ gif.sh
@@ -2,6 +2,7 @@
 
 # 指定包含图片序列的顶级目录
 top_dir="./image_frame/"
+# 输出目录
 out_dir="./output_raw"
 
 # 遍历顶级目录中的所有子目录
@@ -13,7 +14,7 @@ for dir in "$top_dir"/*; do
     # 构建输出文件名，假设目录名即为你想要输出的.gif文件名
     output_file="$out_dir/$dir_name.gif"
 
-    # 执行ffmpeg命令
+    # 执行ffmpeg命令 帧率24、加速2x、固定400高度进行居中裁剪
     ffmpeg -framerate 24 -i "$dir/frame_%03d.png" -vf "setpts=.5*PTS,scale=w=-1:h=400,crop=w=304:h=400" "$output_file"
   fi
 done
